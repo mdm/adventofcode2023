198f760a62f756ed84f6a7be6f99feb55fd64220
Marc Dominik Migge <marcmigge@gmx.net>
2024-06-21 12:37:00 +0200

    Clean up day 21

--- day21/lib/day21.ml
+++ day21/lib/day21.ml
@@ -24,10 +24,8 @@ let neighbors puzzle on_grid x y =
   [(0, -1); (1, 0); (0, 1); (-1, 0);]
   |> List.map ~f:(fun (offset_x, offset_y) -> (x + offset_x, y + offset_y))
   |> List.filter ~f:(fun (neighbor_x, neighbor_y) -> on_grid puzzle neighbor_x neighbor_y)
-  (* |> List.map ~f:(fun (neighbor_x, neighbor_y) -> Stdio.printf "Neighbor %d, %d\n" neighbor_x neighbor_y; (neighbor_x, neighbor_y)) *)
 
 let bfs map init add on_grid start limit =
-  (* Stdio.printf "Starting BFS\n"; *)
   let width = Array.length map.(0) in
   let height = Array.length map in
   let visited = Hashtbl.create (module String) in
@@ -42,7 +40,6 @@ let bfs map init add on_grid start limit =
     match front with
       Some((current_x, current_y)) -> 
         let distance = Hashtbl.find_exn visited (key current_x current_y) in
-        (* Stdio.printf "Visiting %d, %d, %d\n" current_x current_y distance; *)
         let next = if distance < limit then
           neighbors map on_grid current_x current_y
         else
@@ -63,28 +60,16 @@ let part1 puzzle =
   |> List.filter ~f:(fun (_, _, d) -> d % 2 = 64 % 2)
   |> List.length
   |> Int.to_string
-  
-let part2 puzzle =
-  let target = 26501365 in
-  (* let target = 131 * 5 + 65 in *)
-  (* let target = 3 * 3 + 1 in *)
-  let (map, start) = puzzle in
+
+let precalc map start =
   let width = Array.length map.(0) in
   let height = Array.length map in
   let (start_x, start_y) = start in
-  (* Stdio.printf "************************************\n"; *)
-  (* let naive = bfs map [] (fun acc x y d -> (x, y, d)::acc) (fun _ _ _  -> true) start target
-              |> List.filter ~f:(fun (_, _, d) -> d % 2 = target % 2)
-              |> List.sort ~compare:(fun (x1, y1, _) (x2, y2, _) -> Int.compare (y1 * width + x1) (y2 * width + x2))
-              (* |> List.map ~f:(fun (x, y, d) -> Stdio.printf "Naive: %d, %d, %d\n" (x - start_x) (y - start_y) d; d) *)
-              |> List.length
-  in *)
   let init () = Hashtbl.create (module Int) in
   let add acc _x _y d = Hashtbl.update acc d ~f:(fun old -> 1 + Option.value old ~default:0); acc in
-  let lookups = Array.of_list [
+  let lookups = [
     bfs map (init ()) add on_grid (width - 1, height - 1) (width * height) (* north west quadrant *)
     |> Hashtbl.to_alist;
-    (* |> List.map ~f:(fun (d, count) -> Stdio.printf "North west: %d, %d\n" d count; (d, count)); *)
     bfs map (init ()) add on_grid (start_x, height - 1) (width * height) (* north strip *)
     |> Hashtbl.to_alist;
     bfs map (init ()) add on_grid (0, height - 1) (width * height) (* north east quadrant *)
@@ -103,7 +88,7 @@ let part2 puzzle =
     |> Hashtbl.to_alist;
   ] in
   let offsets = bfs map [] (fun acc x y d -> (x, y, d)::acc) on_grid start (width * height) in
-  let offsets = Array.of_list [
+  let offsets = [
     1 + (List.find_exn offsets ~f:(fun (x, y, _) -> x = 0 && y = 0) |> (fun (_, _, d) -> d));
     0 + (List.find_exn offsets ~f:(fun (x, y, _) -> x = start_x && y = 0) |> (fun (_, _, d) -> d));
     1 + (List.find_exn offsets ~f:(fun (x, y, _) -> x = width - 1 && y = 0) |> (fun (_, _, d) -> d));
@@ -114,81 +99,82 @@ let part2 puzzle =
     0 + (List.find_exn offsets ~f:(fun (x, y, _) -> x = start_x && y = height - 1) |> (fun (_, _, d) -> d));
     1 + (List.find_exn offsets ~f:(fun (x, y, _) -> x = width - 1 && y = height - 1) |> (fun (_, _, d) -> d));
   ] in
-  let quadrant lookup offset =
-    let full_odd = List.filter lookup ~f:(fun (d, _) -> not (d % 2 = target % 2))
-                   |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let full_even = List.filter lookup ~f:(fun (d, _) -> d % 2 = target % 2)
-                    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let size = (target - offset) / height - 1 in
-    let rest_upper = (target - offset) % height in
-    let rest_lower = (target - offset) % height + height in
-    let partial_upper_odd = List.filter lookup ~f:(fun (d, _) -> d < rest_upper && not (d % 2 = target % 2))
-                           |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let partial_upper_even = List.filter lookup ~f:(fun (d, _) -> d < rest_upper && d % 2 = target % 2)
-                            |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let partial_lower_odd = List.filter lookup ~f:(fun (d, _) -> d < rest_lower && not (d % 2 = target % 2))
-                           |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let partial_lower_even = List.filter lookup ~f:(fun (d, _) -> d < rest_lower && d % 2 = target % 2)
-                            |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    (* Stdio.printf "size: %d, rest_upper: %d, rest_lower: %d\n" size rest_upper rest_lower;
-    Stdio.printf "full_odd: %d, full_even: %d\n" full_odd full_even;
-    Stdio.printf "partial_upper_odd: %d, partial_upper_even: %d\n" partial_upper_odd partial_upper_even;
-    Stdio.printf "partial_lower_odd: %d, partial_lower_even: %d\n" partial_lower_odd partial_lower_even; *)
+  List.zip_exn lookups offsets
+
+  let full_odd lookup target =
+    List.filter lookup ~f:(fun (d, _) -> not (d % 2 = target % 2))
+    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
+
+  let full_even lookup target =
+    List.filter lookup ~f:(fun (d, _) -> d % 2 = target % 2)
+    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
+
+  let size target offset height = (target - offset) / height - 1
+  let rest_upper target offset height = (target - offset) % height
+  let rest_lower target offset height = (target - offset) % height + height
+  let partial_upper_odd lookup rest_upper target =
+    List.filter lookup ~f:(fun (d, _) -> d < rest_upper && not (d % 2 = target % 2))
+    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
+
+  let partial_upper_even lookup rest_upper target =
+    List.filter lookup ~f:(fun (d, _) -> d < rest_upper && d % 2 = target % 2)
+      |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
+
+  let partial_lower_odd lookup rest_lower target =
+    List.filter lookup ~f:(fun (d, _) -> d < rest_lower && not (d % 2 = target % 2))
+    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
+
+  let partial_lower_even lookup rest_lower target =
+    List.filter lookup ~f:(fun (d, _) -> d < rest_lower && d % 2 = target % 2)
+    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
+
+  let quadrant lookup offset height target =
+    let full_odd = full_odd lookup target in
+    let full_even = full_even lookup target in
+    let size = size target offset height in
+    let rest_upper = rest_upper target offset height in
+    let rest_lower = rest_lower target offset height in
+    let partial_upper_odd = partial_upper_odd lookup rest_upper target in
+    let partial_upper_even = partial_upper_even lookup rest_upper target in
+    let partial_lower_odd = partial_lower_odd lookup rest_lower target in
+    let partial_lower_even = partial_lower_even lookup rest_lower target in
     if size % 2 = 0 then
       ((size / 2) ** 2) * full_even + ((size / 2) * (size / 2 + 1)) * full_odd + (size + 1) * partial_lower_even + (size + 2) * partial_upper_odd
     else
       ((size / 2 + 1) ** 2) * full_even + ((size / 2) * (size / 2 + 1)) * full_odd + (size + 1) * partial_lower_odd + (size + 2) * partial_upper_even
-  in
-  let strip lookup offset =
-    let full_odd = List.filter lookup ~f:(fun (d, _) -> not (d % 2 = target % 2))
-                   |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let full_even = List.filter lookup ~f:(fun (d, _) -> d % 2 = target % 2)
-                    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let size = (target - offset) / height - 1 in
-    let rest_upper = (target - offset) % height in
-    let rest_lower = (target - offset) % height + height in
-    let partial_upper_odd = List.filter lookup ~f:(fun (d, _) -> d < rest_upper && not (d % 2 = target % 2))
-                           |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let partial_upper_even = List.filter lookup ~f:(fun (d, _) -> d < rest_upper && d % 2 = target % 2)
-                            |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let partial_lower_odd = List.filter lookup ~f:(fun (d, _) -> d < rest_lower && not (d % 2 = target % 2))
-                           |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    let partial_lower_even = List.filter lookup ~f:(fun (d, _) -> d < rest_lower && d % 2 = target % 2)
-                            |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count)
-    in
-    (* Stdio.printf "size: %d, rest_upper: %d, rest_lower: %d\n" size rest_upper rest_lower;
-    Stdio.printf "full_odd: %d, full_even: %d\n" full_odd full_even;
-    Stdio.printf "partial_upper_odd: %d, partial_upper_even: %d\n" partial_upper_odd partial_upper_even;
-    Stdio.printf "partial_lower_odd: %d, partial_lower_even: %d\n" partial_lower_odd partial_lower_even; *)
+
+  let strip lookup offset height target =
+    let full_odd = full_odd lookup target in
+    let full_even = full_even lookup target in
+    let size = size target offset height in
+    let rest_upper = rest_upper target offset height in
+    let rest_lower = rest_lower target offset height in
+    let partial_upper_odd = partial_upper_odd lookup rest_upper target in
+    let partial_upper_even = partial_upper_even lookup rest_upper target in
+    let partial_lower_odd = partial_lower_odd lookup rest_lower target in
+    let partial_lower_even = partial_lower_even lookup rest_lower target in
     if size % 2 = 0 then
       (size / 2) * full_even + (size / 2) * full_odd + partial_lower_even + partial_upper_odd
     else
       (size / 2 + 1) * full_even + (size / 2) * full_odd + partial_lower_odd + partial_upper_even
-  in
-  (* Stdio.printf "Reference %d\n" naive; *)
-  Stdio.printf "%d x %d: (%d, %d)\n" width height start_x start_y;
-  [
-    quadrant lookups.(0) offsets.(0);
-    strip lookups.(1) offsets.(1);
-    quadrant lookups.(2) offsets.(2);
-    strip lookups.(3) offsets.(3);
-    List.filter lookups.(4) ~f:(fun (d, _) -> d % 2 = target % 2)
-    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count);
-    strip lookups.(5) offsets.(5);
-    quadrant lookups.(6) offsets.(6);
-    strip lookups.(7) offsets.(7);
-    quadrant lookups.(8) offsets.(8);
-  ]
+
+let part2 puzzle =
+  let target = 26501365 in
+  let (map, start) = puzzle in
+  let height = Array.length map in
+  let data = precalc map start in
+  List.zip_exn [
+    quadrant;
+    strip;
+    quadrant;
+    strip;
+    (fun lookup _offset _height _target -> List.filter lookup ~f:(fun (d, _) -> d % 2 = target % 2)
+    |> List.fold ~init:0 ~f:(fun acc (_, count) -> acc + count));
+    strip;
+    quadrant;
+    strip;
+    quadrant;
+  ] data
+  |> List.map ~f:(fun (f, (lookup, offset)) -> f lookup offset height target)
   |> List.fold ~init:0 ~f:(+)
   |> Int.to_string
