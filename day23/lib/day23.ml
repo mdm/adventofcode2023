bd77549ed5502e5af3cefe3552b3d2548d0dbdab
Marc Dominik Migge <marcmigge@gmx.net>
2024-06-26 13:08:17 +0200

    Finish day 23

--- day23/lib/day23.ml
+++ day23/lib/day23.ml
@@ -39,9 +39,9 @@ let neighbors map on_grid x y =
   |> List.map ~f:(fun (offset_x, offset_y) -> (x + offset_x, y + offset_y))
   |> List.filter ~f:(fun (neighbor_x, neighbor_y) -> on_grid map neighbor_x neighbor_y)
 
-let bfs map init add on_grid start limit =
-  let width = Array.length map.(0) in
-  let height = Array.length map in
+let pair_equal (x1, y1) (x2, y2) = x1 = x2 && y1 = y2
+
+let bfs map on_grid start nodes use_slopes =
   let visited = Hashtbl.create (module String) in
   let key x y = Int.to_string x ^ "," ^ Int.to_string y in  
   let queue = Queue.create () in
@@ -49,24 +49,27 @@ let bfs map init add on_grid start limit =
     `Ok -> Queue.enqueue queue (neighbor_x, neighbor_y)
   | `Duplicate -> ()
   in
+  let add acc x y distance = {destination = (x, y); distance}::acc in
   let rec bfs_inner acc =
     let front = Queue.dequeue queue in
     match front with
       Some((current_x, current_y)) -> 
         let distance = Hashtbl.find_exn visited (key current_x current_y) in
-        let next = if distance < limit then
-          neighbors map on_grid current_x current_y
+        if not (pair_equal (current_x, current_y) start) && List.mem nodes (current_x, current_y) ~equal:pair_equal then
+          bfs_inner (add acc current_x current_y distance)
         else
-          []
-        in
-        next
-        |> List.filter ~f:(fun (neighbor_x, neighbor_y) -> not (Char.equal map.(neighbor_y % height).(neighbor_x % width) '#'))
-        |> List.iter ~f:(update (distance + 1));
-        bfs_inner (add acc current_x current_y distance)
+          (neighbors map on_grid current_x current_y
+          |> List.filter ~f:(fun (neighbor_x, neighbor_y) -> not (Char.equal map.(neighbor_y).(neighbor_x) '#'))
+          |> List.filter ~f:(fun (neighbor_x, neighbor_y) -> use_slopes || not (Char.equal map.(neighbor_y).(neighbor_x) '>' && neighbor_x <= current_x))
+          |> List.filter ~f:(fun (neighbor_x, neighbor_y) -> use_slopes || not (Char.equal map.(neighbor_y).(neighbor_x) '<' && neighbor_x >= current_x))
+          |> List.filter ~f:(fun (neighbor_x, neighbor_y) -> use_slopes || not (Char.equal map.(neighbor_y).(neighbor_x) 'v' && neighbor_y <= current_y))
+          |> List.filter ~f:(fun (neighbor_x, neighbor_y) -> use_slopes || not (Char.equal map.(neighbor_y).(neighbor_x) '^' && neighbor_y >= current_y))
+          |> List.iter ~f:(update (distance + 1));
+          bfs_inner acc)
     | None -> acc
   in
   update 0 start;
-  bfs_inner init
+  bfs_inner []
 
 let is_walkable map x y = Char.equal map.(y).(x) '#' |> not
 
@@ -82,17 +85,36 @@ let find_nodes map =
   |> Array.to_list
   |> List.concat
 
+let add_edges map nodes use_slopes =
+  List.map nodes ~f:(fun (x, y) -> {position = (x, y); edges = bfs map on_grid (x, y) nodes use_slopes})
+
+let longest_path nodes start goal =
+  let node_of_pos pos = List.find_exn nodes ~f:(fun n -> pair_equal n.position pos) in
+  let rec longest_path_inner visited current distance =
+    if pair_equal current.position goal then
+      distance
+    else
+      let visited = current::visited in
+      current.edges
+      |> List.filter ~f:(fun edge -> not (List.exists visited ~f:(fun n -> pair_equal n.position edge.destination)))
+      |> List.map  ~f:(fun edge -> longest_path_inner visited (node_of_pos edge.destination) (distance + edge.distance))
+      |> List.max_elt ~compare:Int.compare
+      |> Option.value ~default:0
+  in
+  longest_path_inner [] (node_of_pos start) 0
+
 let part1 puzzle =
-  let (map, start, _goal) = puzzle in
+  let (map, start, goal) = puzzle in
   let nodes = find_nodes map in
-  List.iter nodes ~f:(fun (x, y) -> Stdio.printf "(%d, %d)\n" x y);
-  Stdio.printf "# nodes: %d\n" (List.length nodes);
-  Stdio.printf "map size: %d x %d\n" (Array.length map.(0)) (Array.length map);
-  bfs map [] (fun acc x y d -> (x, y, d)::acc) on_grid start 64
-  |> List.filter ~f:(fun (_, _, d) -> d % 2 = 64 % 2)
-  |> List.length
+  let nodes = List.concat [nodes; [start; goal]] in
+  let nodes = add_edges map nodes false in
+  longest_path nodes start goal
   |> Int.to_string
 
-let part2 _puzzle =
-  0
+let part2 puzzle =
+  let (map, start, goal) = puzzle in
+  let nodes = find_nodes map in
+  let nodes = List.concat [nodes; [start; goal]] in
+  let nodes = add_edges map nodes true in
+  longest_path nodes start goal
   |> Int.to_string
