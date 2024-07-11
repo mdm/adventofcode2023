67717bbf358895b5f90b79003f119c9f2e6a4b71
Marc Dominik Migge <marcmigge@gmx.net>
2024-07-11 21:35:48 +0200

    Finish day 24

--- day24/lib/day24.ml
+++ day24/lib/day24.ml
@@ -1,4 +1,5 @@
 open Base
+open Z3
 
 type vec3 = {
   x: int;
@@ -51,6 +52,40 @@ let part1 puzzle =
   |> List.length
   |> Int.to_string
 
-let part2 _puzzle =
-  0
+let solve puzzle =
+  let ctx = mk_context [] in
+  let solver = Solver.mk_solver ctx None in
+  let get_value model var = Model.eval model var true |> Option.value_exn |> Expr.to_string in
+  let zero = Expr.mk_numeral_int ctx 0 (Arithmetic.Integer.mk_sort ctx) in
+  let spx = Arithmetic.Integer.mk_const_s ctx "spx" in
+  let spy = Arithmetic.Integer.mk_const_s ctx "spy" in
+  let spz = Arithmetic.Integer.mk_const_s ctx "spz" in
+  let svx = Arithmetic.Integer.mk_const_s ctx "svx" in
+  let svy = Arithmetic.Integer.mk_const_s ctx "svy" in
+  let svz = Arithmetic.Integer.mk_const_s ctx "svz" in
+  let hailstone_constraint i h =
+    let hpx = Expr.mk_numeral_int ctx h.position.x (Arithmetic.Integer.mk_sort ctx) in
+    let hpy = Expr.mk_numeral_int ctx h.position.y (Arithmetic.Integer.mk_sort ctx) in
+    let hpz = Expr.mk_numeral_int ctx h.position.z (Arithmetic.Integer.mk_sort ctx) in
+    let hvx = Expr.mk_numeral_int ctx h.velocity.x (Arithmetic.Integer.mk_sort ctx) in
+    let hvy = Expr.mk_numeral_int ctx h.velocity.y (Arithmetic.Integer.mk_sort ctx) in
+    let hvz = Expr.mk_numeral_int ctx h.velocity.z (Arithmetic.Integer.mk_sort ctx) in
+    let t = Arithmetic.Integer.mk_const_s ctx (Printf.sprintf "t_%d" i) in
+    let t_ge_zero = Arithmetic.mk_ge ctx t zero in
+    let eqx = Boolean.mk_eq ctx (Arithmetic.mk_add ctx [hpx; Arithmetic.mk_mul ctx [t; hvx]]) (Arithmetic.mk_add ctx [spx; Arithmetic.mk_mul ctx [t; svx]]) in
+    let eqy = Boolean.mk_eq ctx (Arithmetic.mk_add ctx [hpy; Arithmetic.mk_mul ctx [t; hvy]]) (Arithmetic.mk_add ctx [spy; Arithmetic.mk_mul ctx [t; svy]]) in
+    let eqz = Boolean.mk_eq ctx (Arithmetic.mk_add ctx [hpz; Arithmetic.mk_mul ctx [t; hvz]]) (Arithmetic.mk_add ctx [spz; Arithmetic.mk_mul ctx [t; svz]]) in
+    Boolean.mk_and ctx [t_ge_zero; eqx; eqy; eqz]
+  in
+  puzzle
+  |> List.mapi ~f:hailstone_constraint
+  |> List.iter ~f:(fun c -> Solver.add solver [c]);
+  match Solver.check solver [] with
+  | UNSATISFIABLE -> assert false
+  | UNKNOWN -> assert false
+  | SATISFIABLE -> let model = Solver.get_model solver |> Option.value_exn in List.fold [spx; spy; spz] ~init:0 ~f:(fun acc var -> acc + (get_value model var |> Int.of_string))
+
+
+let part2 puzzle =
+  solve puzzle
   |> Int.to_string
